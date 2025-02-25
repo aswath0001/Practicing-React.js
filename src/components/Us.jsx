deab2c5a80b0c5b73ae5ad79fef5c17bb97aa431
aswath0001 <rsaswaths38@gmail.com>
2025-02-25 17:14:38 +0530

    25/02/2025

--- src/components/Us.jsx
+++ src/components/Us.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { useState } from 'react';
-
+     {/*  use state is bascially a hook 
+          A hook is simply a javascript function
+          Each hook starts with a "use"
+          Use state hook simply returns an array which contains variable and function
+    */}
 const Us = () => {
-    let x = 1;
+    const [id, setId] = useState (1);
+
+
+    
+
     const increment = () => {
-        x = x+1;
-        console.log("x",x)
+       /* x = x+1;
+        console.log("x",x)*/
+        setId (id+1);
     };
   return (
     <div>
         <h1>
-        {x}
+        {id}
         </h1>
         <button onClick={increment}>Click </button>
     </div>
