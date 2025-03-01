c8c9ba2ec7f69ec6e2a0e540a5876377d46cf21c
aswath0001 <rsaswaths38@gmail.com>
2025-03-01 14:07:28 +0530

    01/03/2025

--- src/components/Gsap/GSAPDemo.jsx
+++ src/components/Gsap/GSAPDemo.jsx
@@ -1,31 +1,26 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
 const GSAPDemo = () => {
     const Dref = useRef(null);
-    useGSAP (() => {
-        gsap.to(Dref.current,{
-            y:30,
-            duration : 0.5,
-            yoyo : true,
-            ease:'power1.inOut',
-            onComplete :() => {
-                gsap.to (Dref.current,{
-                    y:0,
-                    duration: 0.5,
-                    ease:'power1.inOut'
-                })
-            }
-        }
+    const Dref2 = useRef(null);
 
-        )
-    })
-  return (
-    <div >
-       <h1 ref={Dref}>Hello</h1>
-    </div>
-  )
-}
+    useGSAP(() => {
+        gsap.timeline()
+            .to(Dref.current, { y: 30, duration: 0.5, ease: "power1.inOut" })
+            .to(Dref.current, { y: 0, duration: 0.5, ease: "power1.inOut" });
 
-export default GSAPDemo
+        gsap.timeline()
+            .to(Dref2.current, { x: 30, duration: 0.7, ease: "power1.inOut" })
+            .to(Dref2.current, { x: 0, duration: 0.7, ease: "power1.inOut" });
+    });
+
+    return (
+        <div>
+            <h1 ref={Dref}>Hello <span ref={Dref2}>world</span></h1>
+        </div>
+    );
+};
+
+export default GSAPDemo;
