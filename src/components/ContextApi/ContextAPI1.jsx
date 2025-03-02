74e59ca013f77cda8e79f6eadb6fe96d7d7d0b83
aswath0001 <rsaswaths38@gmail.com>
2025-03-02 11:17:45 +0530

    02/03/2025

--- src/components/ContextApi/ContextAPI1.jsx
+++ src/components/ContextApi/ContextAPI1.jsx
@@ -3,14 +3,18 @@ import { createContext } from 'react'
 import ContextAPI2 from './ContextAPI2'
 
 export const Data = createContext()
-
+export const Data1 = createContext()
 const ContextAPI1 = () => {
     const name = 'aswath';
+    const age = '22';
   return (
     <div>
         <Data.Provider value ={name}>
             <ContextAPI2/>
         </Data.Provider>
+        <Data1.Provider value ={age}>
+            <ContextAPI2/>
+        </Data1.Provider>
     </div>
   )
 }
--- src/components/ContextApi/ContextAPI2.jsx
+++ src/components/ContextApi/ContextAPI2.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Data } from './ContextAPI1'
+import { Data } from './ContextAPI1';
+import { Data1 } from './ContextAPI1';
 
 const ContextAPI2 = () => {
   return (
+    <div>
     <Data.Consumer>
      {(name) => {
         return<h1>{name}</h1>
      }}
     </Data.Consumer>
+    <Data1.Consumer>
+     {(age) => {
+        return<h1>{age}</h1>
+     }}
+    </Data1.Consumer>
+    </div>
   )
 }
 
